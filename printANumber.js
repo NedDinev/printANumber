2155624d7a8869657b97f3cf383dfc85835d6913
Nedelcho Dinev <ned.dinev@gmail.com>
2021-10-14 13:31:23 +0300

    Add files via upload

---
+++ printANumber.js
@@ -0,0 +1,29 @@
+function printANumber(number) {
+  number = Number(number);
+  if (number == 1) {
+    console.log(`one`);
+  } else if (number == 2) {
+    console.log(`two`);
+  } else if (number == 3) {
+    console.log(`three`);
+  } else if (number == 4) {
+    console.log(`four`);
+  } else if (number == 5) {
+    console.log(`five`);
+  } else if (number == 6) {
+    console.log(`six`);
+  } else if (number == 7) {
+    console.log(`seven`);
+  } else if (number == 8) {
+    console.log(`eight`);
+  } else if (number == 9) {
+    console.log(`nine`);
+  } else if (number >= 10) {
+    console.log(`number too big`);
+  }
+}
+
+printANumber(5);
+printANumber(1);
+printANumber(9);
+printANumber(10);
